756bfefaccbd241c6c109725e1a5324d37201e28
Mard <bengilreymard@gmail.com>
2026-02-05 14:39:53 +0800

    added bind for hosting backend

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn MainBackend.wsgi:application
+gunicorn MainBackend.wsgi:application --bind 0.0.0.0:8000
