008d73d0502d3b4ba5c74bf24dc0d838e298f0b0
Mard <bengilreymard@gmail.com>
2026-02-05 15:42:57 +0800

    fix 400 error

--- backend/MainBackend/settings.py
+++ backend/MainBackend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['mechconnectse2-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
